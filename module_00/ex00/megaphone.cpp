5f88ed15c53c4eadc447c38936d0df1edb7550e6
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-02 16:06:53 +0200

    indentation using spaces

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -2,21 +2,21 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 1)
+    if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			int j = 0;
-			while (argv[i][j] != '\0')
-			{
-				std::cout << (char)std::toupper(argv[i][j]);
-				j++;
-			}
-			if (i < argc - 1)
-				std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            int j = 0;
+            while (argv[i][j] != '\0')
+            {
+                std::cout << (char)std::toupper(argv[i][j]);
+                j++;
+            }
+            if (i < argc - 1)
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
 }
