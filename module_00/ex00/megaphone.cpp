a40768cb70e0668c74b81074171d99c6a338b064
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-06 19:23:11 +0200

    applied linting rules

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc == 1)
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        for (int i = 1; i < argc; i++)
-        {
+    }
+    else {
+        for (int i = 1; i < argc; i++) {
             int j = 0;
-            while (argv[i][j] != '\0')
-            {
+            while (argv[i][j] != '\0') {
                 std::cout << (char)std::toupper(argv[i][j]);
                 j++;
             }
