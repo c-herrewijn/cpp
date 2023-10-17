1b3fa1aa6828ab5f48e932f8f0e6e378f1250244
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-17 23:21:50 +0200

    fix: 2nd argument cannot be an empty string

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,9 +6,11 @@ std::string findReplace(std::string inStr, std::string find,
                         std::string replaceBy)
 {
     size_t findLen = find.length();
+    if (findLen == 0) {
+        return (inStr);
+    }
     std::string outStr = "";
     std::string subString;
-
     for (size_t i=0; i<inStr.length(); i++) {
         subString = inStr.substr(i, findLen);
         if (i + findLen > inStr.length()) {
@@ -29,15 +31,20 @@ int main(int argc, char *argv[])
 {
     // parsing inputs
     if (argc != 4) {
-        std::cout << "invalid number of arguments" << std::endl;
+        std::cout << "Error: invalid number of arguments" << std::endl;
         return (1);
     }
     std::string filename = argv[1];
     std::string s1 = argv[2];
     std::string s2 = argv[3];
     std::ifstream inFile(filename);
+    if (s1.empty()) {
+        std::cout << "Error: the second argument can not be an empty string"
+                  << std::endl;
+        return (1);
+    }
     if (!inFile) {
-        std::cout << "file " << filename << " does not exist" << std::endl;
+        std::cout << "Error: file " << filename << " does not exist" << std::endl;
         return (1);
     }
 
