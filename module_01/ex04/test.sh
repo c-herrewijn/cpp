6577010256aa67f32aa15217df7e090def0a6e5e
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-18 14:57:26 +0200

    added test cases

--- module_01/ex04/test.sh
+++ module_01/ex04/test.sh
@@ -4,23 +4,61 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# test case 1:
-fileName="file1"
+execute_test()
+{
+  fileName="file${test_case_nr}"
+  echo $original > $fileName
+  echo $expected > "${fileName}.expected"
+  ./findreplace "$fileName" "$findStr" "$replaceStr"
+  res=$(diff "${fileName}.expected" "${fileName}.replace" | wc -l);
+  if [ "$res" -eq "0" ]; then
+    printf "case${test_case_nr}: ${GREEN}[OK]${NC}\n"
+  else
+    printf "case${test_case_nr}: ${RED}[NOK]${NC}\n"
+  fi
+  rm $fileName
+  rm "${fileName}.expected"
+  rm "${fileName}.replace"
+}
+
+test_case_nr=1
 findStr="hello"
 replaceStr="goodbye"
-original="hello1\nhello2\nhello3\n";
-expected="goodbye1\ngoodbye2\ngoodbye3\n";
+original="hello1\nhello2\nhello3\n"
+expected="goodbye1\ngoodbye2\ngoodbye3\n"
+execute_test;
 
-echo $original > $fileName;
-echo $expected > "${fileName}.expected";
-./findreplace file1 $findStr $replaceStr;
-res=$(diff "${fileName}.expected" "${fileName}.replace" | wc -l);
-if [ "$res" -eq "0" ]; then
-  printf "case1: ${GREEN}[OK]${NC}\n"
-else
-  printf "case1: ${RED}[NOK]${NC}\n"
-fi
-rm $fileName
-rm "${fileName}.expected"
-rm "${fileName}.replace"
+test_case_nr=2
+findStr=" "
+replaceStr="."
+original="hello there\how are you\n  extra spaces  \n  "
+expected="hello.there\how.are.you\n..extra.spaces..\n.."
+execute_test;
 
+test_case_nr=3
+findStr="hello"
+replaceStr="goodbye"
+original="11hello\n11hellohello"
+expected="11goodbye\n11goodbyegoodbye"
+execute_test;
+
+test_case_nr=4
+findStr="aa"
+replaceStr="b"
+original="aaaaa";
+expected="bba";
+execute_test;
+
+test_case_nr=5
+findStr="hello"
+replaceStr="goodbye"
+original="hello"
+expected="goodbye"
+execute_test;
+
+test_case_nr=6
+findStr="hello"
+replaceStr=""
+original="hellohello"
+expected=""
+execute_test;
