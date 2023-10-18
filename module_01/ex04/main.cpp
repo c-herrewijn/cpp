3e64f60e96884db424068bafff955f7674737559
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-18 16:05:45 +0200

    moved input validation in separate function

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -27,28 +27,40 @@ std::string findReplace(std::string inStr, std::string find,
     return (outStr);
 }
 
-int main(int argc, char *argv[])
+bool parseInput(int argc, char *argv[],
+                std::string &filename, std::string &s1,
+                std::string &s2, std::ifstream &inFile)
 {
-    // parsing inputs
     if (argc != 4) {
         std::cout << "Error: invalid number of arguments" << std::endl;
-        return (1);
+        return (false);
     }
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
-    std::ifstream inFile(filename);
+    filename = argv[1];
+    s1 = argv[2];
+    s2 = argv[3];
+    inFile.open(filename);
+    if (!inFile) {
+        std::cout << "Error: file " << filename << " does not exist" << std::endl;
+        return (false);
+    }
     if (s1.empty()) {
         std::cout << "Error: the second argument can not be an empty string"
                   << std::endl;
-        return (1);
+        return (false);
     }
-    if (!inFile) {
-        std::cout << "Error: file " << filename << " does not exist" << std::endl;
-        return (1);
-    }
+    return (true);
+}
 
-    // create output file
+int main(int argc, char *argv[])
+{
+    std::ifstream inFile;
+    std::string filename;
+    std::string s1;
+    std::string s2;
+
+    if (!parseInput(argc, argv, filename, s1, s2, inFile)) {
+        return (1);
+    }
     std::ofstream outFile(filename + ".replace");
     std::string lineIn;
     while (std::getline(inFile, lineIn)) {
