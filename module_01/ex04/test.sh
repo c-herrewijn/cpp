5a4b4c50767d60c1ff27327e450a3f1d03cd654e
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-18 15:14:43 +0200

    fixed testcases

--- module_01/ex04/test.sh
+++ module_01/ex04/test.sh
@@ -7,10 +7,10 @@ NC='\033[0m' # No Color
 execute_test()
 {
   fileName="file${test_case_nr}"
-  echo $original > $fileName
-  echo $expected > "${fileName}.expected"
+  echo -e "$original" > $fileName
+  echo -e "$expected" > "${fileName}.expected"
   ./findreplace "$fileName" "$findStr" "$replaceStr"
-  res=$(diff "${fileName}.expected" "${fileName}.replace" | wc -l);
+  res=$(diff "${fileName}.expected" "${fileName}.replace" | wc -l)
   if [ "$res" -eq "0" ]; then
     printf "case${test_case_nr}: ${GREEN}[OK]${NC}\n"
   else
@@ -26,39 +26,39 @@ findStr="hello"
 replaceStr="goodbye"
 original="hello1\nhello2\nhello3\n"
 expected="goodbye1\ngoodbye2\ngoodbye3\n"
-execute_test;
+execute_test
 
 test_case_nr=2
 findStr=" "
 replaceStr="."
-original="hello there\how are you\n  extra spaces  \n  "
-expected="hello.there\how.are.you\n..extra.spaces..\n.."
-execute_test;
+original="hello there\nhow are you\n  extra spaces  \n  "
+expected="hello.there\nhow.are.you\n..extra.spaces..\n.."
+execute_test
 
 test_case_nr=3
 findStr="hello"
 replaceStr="goodbye"
 original="11hello\n11hellohello"
 expected="11goodbye\n11goodbyegoodbye"
-execute_test;
+execute_test
 
 test_case_nr=4
 findStr="aa"
 replaceStr="b"
-original="aaaaa";
-expected="bba";
-execute_test;
+original="aaaaa"
+expected="bba"
+execute_test
 
 test_case_nr=5
 findStr="hello"
 replaceStr="goodbye"
 original="hello"
 expected="goodbye"
-execute_test;
+execute_test
 
 test_case_nr=6
 findStr="hello"
 replaceStr=""
 original="hellohello"
 expected=""
-execute_test;
+execute_test
