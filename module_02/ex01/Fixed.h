e33fafefe92b7310abaa9069e76523f6ac1d4d7e
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-23 21:34:54 +0200

    added ex01

---
+++ module_02/ex01/Fixed.h
@@ -0,0 +1,26 @@
+#ifndef FIXED_H
+#define FIXED_H
+#include <iostream>
+
+class Fixed {
+public:
+    Fixed();
+    Fixed(const int i);
+    Fixed(const float f);
+    Fixed(const Fixed &obj);
+    ~Fixed();
+    Fixed &operator=(const Fixed &obj);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat(void) const;
+    int toInt(void) const;
+
+private:
+    int _raw;
+    static const int _nrFractionalBits;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &obj);
+
+#endif
