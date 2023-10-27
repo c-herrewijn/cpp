539a3260fcca82ae8ea67df26b03619666257e08
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-10-27 16:43:06 +0200

    added tests

--- module_03/ex01/main.cpp
+++ module_03/ex01/main.cpp
@@ -11,5 +11,59 @@ int main()
         a.attack("Monter1");
     }
 
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing taking damage..." << std::endl;
+    ScavTrap b("Beatrix");
+    b.takeDamage(30);
+    b.takeDamage(30);
+    b.takeDamage(30);
+    b.takeDamage(30);
+
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing healing..." << std::endl;
+    ScavTrap c("Christina");
+    c.takeDamage(30);
+    c.takeDamage(30);
+    c.takeDamage(30);
+    c.beRepaired(100);
+    c.takeDamage(30);
+    c.takeDamage(30);
+    c.takeDamage(30);
+    c.takeDamage(30);
+
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing extreme cases..." << std::endl;
+    ScavTrap d("Diana");
+    d.takeDamage(4294967295);  // max unsigned int
+    std::cout << ".. testing unnecessary violence..." << std::endl;
+    d.takeDamage(4294967295);  // max unsigned int
+    std::cout << ".. testing impossible actions..." << std::endl;
+    d.attack("Monter2");
+    d.beRepaired(10);
+
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing default ScavTrap..." << std::endl;
+    ScavTrap xx;
+    xx.attack("Monster3");
+    xx.beRepaired(10);
+    // note: ScavTrap's health can be more than its initial value
+    xx.takeDamage(108);
+
+
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing copying ScavTraps..." << std::endl;
+    ScavTrap clone1("SuperClone");
+    clone1.beRepaired(1000);
+    ScavTrap clone2;
+    clone2 = clone1;
+    clone2.takeDamage(1000);
+    ScavTrap clone3(clone1);
+    clone3.takeDamage(1000);
+
+    std::cout << "--------------" << std::endl;
+    std::cout << "Testing guarding the gate..." << std::endl;
+    clone1.guardGate();
+    std::cout << "--------------" << std::endl;
+    
     return (0);
 }
