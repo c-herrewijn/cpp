9da4efc6aa3da1dab78892acfc0e90153d5082a6
Christiaan Herrewijn <cherrewi@student.codam.nl>
2023-11-08 15:49:33 +0100

    made constructors for Abstract class AMateria protected

--- module_04/ex03/AMateria.h
+++ module_04/ex03/AMateria.h
@@ -6,15 +6,17 @@
 
 class  AMateria {
 public:
-    AMateria();
-    AMateria(std::string const &type);
-    AMateria(const AMateria &obj);
     virtual ~AMateria();
     AMateria &operator=(const AMateria &obj);
 
     std::string const &getType() const;  //Returns the materia type
     virtual AMateria *clone() const = 0;
     virtual void use(ICharacter &target);
+
+protected:
+    AMateria();
+    AMateria(std::string const &type);
+    AMateria(const AMateria &obj);
 
 private:
     std::string const type;
