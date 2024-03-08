e25606cf0b82bcbf14067ec3e3b528f9855ce7c4
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-03-08 19:05:41 +0100

    major restructure

--- module_06/ex00/ScalarConverter.cpp
+++ module_06/ex00/ScalarConverter.cpp
@@ -1,17 +1,17 @@
 #include "ScalarConverter.hpp"
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <cctype>
 #include <limits>
+#include <cmath>
+#include <sstream>
 
 namespace {
-    typedef enum inputType_e {
-        CHAR,
-        INT,
-        FLOAT,
-        DOUBLE,
-        NONE_TYPE
-    } inputType_t;
+    typedef enum floatingType_e {
+        SINGLE_PRECISION,
+        DOUBLE_PRECISION
+    } floatingType_t;
 
     typedef struct conversions_s {
         char c;
@@ -22,72 +22,20 @@ namespace {
         std::string fMessage;
         double d;
         std::string dMessage;
+        int numDigitsBehindDecimalPoint = 0;
     } conversions_t;
+
+    typedef struct floatingStrLiteral_s {
+        floatingType_t type;
+        int numDigits;
+        bool hasRadixPoint;
+        int sign;
+        int numDigitsBehindDecimalPoint;
+    } floatingStrLiteral_t;
 }
 
-// valid chars: single quoted chars, or non-numeric printable chars
-static bool isChar(std::string str, conversions_t &conversions)
+static void convertIntToChar(conversions_t &conversions)
 {
-    char c;
-
-    if (str.length() == 3 && str[0] == '\'' && std::isprint(str[1])
-            && str[2] == '\'') {
-        c = str[1];
-    }
-    else if (str.length() == 1 && std::isprint(str[0])
-             && std::isdigit(str[0]) == false) {
-        c = str[0];
-    }
-    else {
-        return false;
-    }
-    conversions.c = c;
-    return (true);
-}
-
-static bool isInt(std::string str, conversions_t &conversions)
-{
-    int sign = str[0] == '-' ? -1 : 1;
-    if (sign == -1) {
-        str.erase(str.begin());
-    }
-    if (str.length() > std::numeric_limits<int>::digits10 + 1) {
-        return false;
-    }
-    for (char c : str) {
-        if (std::isdigit(c) == false) {
-            return false;
-        }
-    }
-    long li = sign * std::stol(str);
-    if (li > std::numeric_limits<int>::max()
-            || li < std::numeric_limits<int>::min()) {
-        return false;
-    }
-    conversions.i = static_cast<int>(li);
-    return (true);
-}
-
-static bool isFloat(std::string str, conversions_t &conversions)
-{
-    return false;
-}
-
-static bool isDouble(std::string str, conversions_t &conversions)
-{
-    return false;
-}
-
-static void convertChar(std::string scalarStr, conversions_t &conversions)
-{
-    conversions.i = static_cast<int>(conversions.c);
-    conversions.f = static_cast<float>(conversions.c);
-    conversions.d = static_cast<double>(conversions.c);
-}
-
-static void convertInt(std::string scalarStr, conversions_t &conversions)
-{
-
     if (conversions.i > std::numeric_limits<char>::max()
             || conversions.i < std::numeric_limits<char>::min()) {
         conversions.cMessage = "impossible";
@@ -98,85 +46,226 @@ static void convertInt(std::string scalarStr, conversions_t &conversions)
     else {
         conversions.c = static_cast<char>(conversions.i);
     }
-    conversions.f = static_cast<float>(conversions.i);
+}
+
+static void convertIntToFloat(conversions_t &conversions) {
+    float f = static_cast<float>(conversions.i);
+    if (static_cast<double>(f) != static_cast<double>(conversions.i)) {
+        conversions.fMessage = " (integer can not be exactly represented in float)";
+    }
+    conversions.f = f;
+}
+
+static std::string floatingTypeToString(conversions_t &conversions, floatingType_t type) {
+    std::stringstream ss;
+    const int defaultPrecision = static_cast<int>(std::cout.precision());
+    int precision = (conversions.numDigitsBehindDecimalPoint == 0) ? 1 : conversions.numDigitsBehindDecimalPoint;
+
+    if (type == SINGLE_PRECISION)
+        ss << std::fixed << std::setprecision(precision) << conversions.f << std::setprecision(defaultPrecision);
+    else
+        ss << std::fixed << std::setprecision(precision) << conversions.d << std::setprecision(defaultPrecision);
+    return ss.str();
+}
+
+static void convertDoubleToFloat(conversions_t &conversions) {
+    float f = static_cast<float>(conversions.d);
+    conversions.f = f;
+    if (floatingTypeToString(conversions, SINGLE_PRECISION) != floatingTypeToString(conversions, DOUBLE_PRECISION)) {
+        conversions.fMessage = " (differs because exact representation is not possible)";
+    }
+}
+
+static void convertDoubleToInt(conversions_t &conversions)
+{
+    if (conversions.d > std::numeric_limits<int>::max()
+            || conversions.d < std::numeric_limits<int>::min()) {
+        conversions.iMessage = "impossible";
+    }
+    else {
+        conversions.i = static_cast<int>(conversions.d);
+    }
+}
+
+// valid chars: single quoted chars, or non-numeric chars
+static bool parseChar(std::string str, conversions_t &conversions)
+{
+    char c = '\0';
+    bool charInput = false;
+
+    if (str.length() == 3 && str[0] == '\'' && str[2] == '\'') {
+        c = str[1];
+    }
+    else if (str.length() == 1 && std::isdigit(str[0]) == false) {
+        c = str[0];
+    }
+    else {
+        return false;
+    }
+    conversions.c = c;
+    conversions.i = static_cast<int>(conversions.c);
+    conversions.f = static_cast<float>(conversions.c);
+    conversions.d = static_cast<double>(conversions.c);
+    return (true);
+}
+
+static bool parseInt(std::string str, conversions_t &conversions)
+{
+    int sign = str[0] == '-' ? -1 : 1;
+    if (sign == -1) {
+        str.erase(str.begin());
+    }
+    for (char c : str) {
+        if (std::isdigit(c) == false) {
+            return false;
+        }
+    }
+    if (str.length() > std::numeric_limits<int>::digits10 + 1) {
+        return false;
+    }
+    long li = sign * std::stol(str);
+    if (li > std::numeric_limits<int>::max()
+            || li < std::numeric_limits<int>::min()) {
+        return false;
+    }
+
+    // conversions
+    conversions.i = static_cast<int>(li);
+    convertIntToChar(conversions);
+    convertIntToFloat(conversions);
     conversions.d = static_cast<double>(conversions.i);
+    return (true);
 }
 
-static void convertFloat(std::string scalarStr, conversions_t &conversions)
-{
-
+static bool parseNaNOrInf(std::string str, conversions_t &conversions) {
+    if (str == "nan" || str == "inf" || str == "-inf") {
+        if (str == "nan") {
+            conversions.f = std::numeric_limits<float>::quiet_NaN();
+            conversions.d = std::numeric_limits<double>::quiet_NaN();
+        }
+        if (str == "inf") {
+            conversions.f = std::numeric_limits<float>::infinity();
+            conversions.d = std::numeric_limits<double>::infinity();
+        }
+        if (str == "-inf") {
+            conversions.f = -std::numeric_limits<float>::infinity();
+            conversions.d = -std::numeric_limits<double>::infinity();
+        }
+        conversions.cMessage = "impossible";
+        conversions.iMessage = "impossible";
+        return true;
+    }
+    return false;
 }
 
-static void convertDouble(std::string scalarStr, conversions_t &conversions)
+static bool parseDecimalFormat(std::string &str, floatingStrLiteral_t &n)
 {
+    n.numDigits = 0;
+    n.hasRadixPoint = false;
+    n.sign = (str[0] == '-') ? -1 : 1;
+    n.numDigitsBehindDecimalPoint = 0;
 
+    int digits10;
+    if (n.type == SINGLE_PRECISION)
+        digits10 = std::numeric_limits<float>::digits10;
+    else
+        digits10 = std::numeric_limits<double>::digits10;
+
+    if (n.sign == -1) {
+        str.erase(str.begin());
+    }
+    for (char c : str) {
+        if (std::isdigit(c)) {
+            n.numDigits++;
+            if (n.hasRadixPoint == true)
+                n.numDigitsBehindDecimalPoint++;
+        }
+        else if (c == '.' && (n.hasRadixPoint == false))
+            n.hasRadixPoint = true;
+        else
+            return false;
+    }
+    if (n.hasRadixPoint == false || n.numDigits == 0 || n.numDigits > digits10) {
+        return false;
+    }
+    return true;
+}
+
+
+// is decimal number within range of float. NOTE: this does NOT guarantee exact representation
+static bool parseFloatOrDouble(std::string str, conversions_t &conversions)
+{
+    floatingStrLiteral_t num;
+    if (str.back() == 'f') {
+        str.pop_back();
+        num.type = SINGLE_PRECISION;
+    }
+    else
+        num.type = DOUBLE_PRECISION;
+
+    if (parseNaNOrInf(str, conversions))
+        return true;
+
+    if (parseDecimalFormat(str, num)) {
+        conversions.numDigitsBehindDecimalPoint = num.numDigitsBehindDecimalPoint;
+        if (num.type == SINGLE_PRECISION) {
+            conversions.f = static_cast<float>(num.sign) * std::stof(str);
+            conversions.d = static_cast<double>(conversions.f);
+        }
+        else {
+            conversions.d = static_cast<double>(num.sign) * std::stod(str);
+            convertDoubleToFloat(conversions);
+        }
+        convertDoubleToInt(conversions);
+        convertIntToChar(conversions);
+        return true;
+    }
+    return (false);
 }
 
 static void printConversions(conversions_t &conversions)
 {
+    // char
     if (conversions.cMessage == "") {
-        std::cout << "char: '" << conversions.c << "'" << std::endl;
+        if (std::isprint(conversions.c))
+            std::cout << "char: '" << conversions.c << "'" << std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
     }
     else {
         std::cout << "char: " << conversions.cMessage << std::endl;
     }
-    if (conversions.iMessage == "") {
+
+    // int
+    if (conversions.iMessage == "")
         std::cout << "int: " << conversions.i << std::endl;
-    }
-    else {
+    else if (conversions.iMessage == "impossible")
         std::cout << "int: " << conversions.iMessage << std::endl;
-    }
-    if (conversions.fMessage == "") {
-        std::cout << "float: " << conversions.f << std::endl;
-    }
-    else {
-        std::cout << "float: "<< conversions.fMessage << std::endl;
-    }
-    if (conversions.dMessage == "") {
-        std::cout << "double: " << conversions.d << std::endl;
-    }
-    else {
+    else
+        std::cout << "int: " << conversions.i << conversions.iMessage << std::endl;
+
+    // float
+    if (conversions.fMessage == "")
+        std::cout << "float: " << floatingTypeToString(conversions, SINGLE_PRECISION) << "f" << std::endl;
+    else if (conversions.fMessage == "impossible")
+        std::cout << "float: " << conversions.fMessage << std::endl;
+    else
+        std::cout << "float: " << floatingTypeToString(conversions, SINGLE_PRECISION) << "f" << conversions.fMessage << std::endl;
+
+    // double
+    if (conversions.dMessage == "")
+        std::cout << "double: " << floatingTypeToString(conversions, DOUBLE_PRECISION) << std::endl;
+    else
         std::cout << "double: "<< conversions.dMessage << std::endl;
-    }
 }
 
 void ScalarConverter::convert(std::string scalarStr)
 {
-    inputType_t inputType;
     conversions_t conversions = {};
-
-    // general validation
-    if (scalarStr == "") {
-        inputType = NONE_TYPE;
-    }
-
-    // detect input type
-    else if (isChar(scalarStr, conversions)) {
-        inputType = CHAR;
-    }
-    else if (isInt(scalarStr, conversions)) {
-        inputType = INT;
-    }
-    else if (isFloat(scalarStr, conversions)) {
-        inputType = FLOAT;
-    }
-    else if (isDouble(scalarStr, conversions)) {
-        inputType = DOUBLE;
-    }
-    else {
-        inputType = NONE_TYPE;
-    }
-
-    // convert input type
-    if (inputType == CHAR) {
-        convertChar(scalarStr, conversions);
-    }
-    if (inputType == INT) {
-        convertInt(scalarStr, conversions);
-    }
-
-    // print
-    if (inputType != NONE_TYPE) {
+    if (scalarStr != "" && (parseChar(scalarStr, conversions)
+                            || parseInt(scalarStr, conversions)
+                            || parseFloatOrDouble(scalarStr, conversions)))
+    {
         printConversions(conversions);
     }
     else {
--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
+#include <float.h>
+#include <limits.h>
+#include <limits>
+#include <numbers>
+#include <iomanip>
+#include <ios>
 
 #define  ANSI_BLUE  "\x1b[34m"
 #define  ANSI_GREEN  "\x1b[32m"
@@ -22,15 +29,41 @@ int main()
     test("'3'");
     test("*");
     test("\t");
+    test("'\t'");
 
     // integers
     std::cout << ANSI_BLUE << "\nINTEGERS" << ANSI_RESET << std::endl;
     test("0");
     test("-65");
     test("65");
+    test("200");
+    test("16777217"); // smallest int not uniquely representable as float
     test("2147483647"); // int max
     test("-2147483648"); // int min
     test("2147483648"); // int max + 1
     test("-2147483649"); // int min - 1
     test("99999999999999999999999"); // way to big
+    test("9999999999999999999"); // way to big
+
+    // floats
+    std::cout << ANSI_BLUE << "\nvalid FLOATS" << ANSI_RESET << std::endl;
+    test(".1f");
+    test("65.0f");
+    test("-65.0f");
+    test("65.1234f");
+    test("65.12345f");
+    test(".3f");
+    test("2147483648.0f");
+
+    // double
+    std::cout << ANSI_BLUE << "\nDOUBLES" << ANSI_RESET << std::endl;
+    test(".0");
+    test("0.");
+    test("0.0");
+    test("0.1");
+    test("21474.83649");
+    test("16777217.0");
+    test("2147483647.");
+    test("2147483648.");
+    test("2147483649.");
 }
