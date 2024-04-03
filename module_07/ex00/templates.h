455e07615c2ad6a45f05b42ba0b10f62b746cb16
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-04-03 15:27:55 +0200

    added ex00

---
+++ module_07/ex00/templates.h
@@ -0,0 +1,24 @@
+#ifndef TEMPLATES
+# define TEMPLATES
+
+template <typename T>
+void swap(T &x, T &y)
+{
+    T &temp = x;
+    x = y;
+    y = temp;
+}
+
+template <typename T>
+T const min(T const &x, T const &y)
+{
+    return (x < y) ? x : y;
+}
+
+template <typename T>
+T const max(T const &x, T const &y)
+{
+    return (x > y) ? x : y;
+}
+
+#endif
