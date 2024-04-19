f673673c43f3f4f26f42715df21b50d682e63ad2
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-04-19 10:46:59 +0200

    added template function

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -1,4 +1,12 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int valueToFind)
+{
+    return std::find(container.begin(), container.end(), valueToFind);
+}
+
 #endif
