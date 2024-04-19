ca340cdf659a9b14ef70f0707d01202b561c94aa
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-04-19 11:43:56 +0200

    added testcases

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -3,6 +3,11 @@
 
 #include <algorithm>
 
+/*
+returns iterator to the first element in the container that matches valueToFind
+returns iterator.end() if valueToFind is not in the container
+iterator.end() is the position directly after the last element in the vector
+*/
 template <typename T>
 typename T::iterator easyfind(T &container, int valueToFind)
 {
--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -1,5 +1,62 @@
+#include <deque>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <vector>
+
 #include "easyfind.hpp"
 
-int main() {
+#define ANSI_RED   "\x1b[31m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_CYAN  "\x1b[36m"
+#define ANSI_RESET "\x1b[0m"
 
+template <typename T>
+void testCase(T &container, int valueToFind)
+{
+    typename T::iterator it = easyfind(container, valueToFind);
+    if (it != container.end()) {
+        std::cout << "Value found: " << ANSI_GREEN << *it << ANSI_RESET << ", "
+                  << "rest of the container: ";
+        while (++it != container.end()) {
+            std::cout << *it << ", ";
+        }
+        std::cout << std::endl;
+    }
+    else {
+        std::cout << "Value " ANSI_RED << valueToFind << ANSI_RESET
+                  << " not found!" << std::endl;
+    }
+
+}
+
+int main()
+{
+    std::vector<int> myIntVect {10, 20, 30, 20, 10, 50};
+    std::list<int> myIntList {10, 20, 30, 20, 10, 50};
+    std::deque<int> myIntDeque {10, 20, 30, 20, 10, 50};
+
+    // test vector
+    std::cout << ANSI_CYAN << "Testing vector..." << ANSI_RESET << std::endl;
+    testCase(myIntVect, 10);
+    testCase(myIntVect, 20);
+    testCase(myIntVect, 30);
+    testCase(myIntVect, 40);
+    testCase(myIntVect, 50);
+
+    // test list
+    std::cout << ANSI_CYAN << "\nTesting list..." << ANSI_RESET << std::endl;
+    testCase(myIntList, 10);
+    testCase(myIntList, 20);
+    testCase(myIntList, 30);
+    testCase(myIntList, 40);
+    testCase(myIntList, 50);
+
+    // test deque
+    std::cout << ANSI_CYAN << "\nTesting deque..." << ANSI_RESET << std::endl;
+    testCase(myIntDeque, 10);
+    testCase(myIntDeque, 20);
+    testCase(myIntDeque, 30);
+    testCase(myIntDeque, 40);
+    testCase(myIntDeque, 50);
 }
