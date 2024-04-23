4209ce438eff4ff2e12f1612e7085a2e59a54e6a
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-04-23 14:11:30 +0200

    added basic tests

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -3,8 +3,37 @@
 
 #include "MutantStack.hpp"
 
+#define ANSI_RED   "\x1b[31m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_CYAN  "\x1b[36m"
+#define ANSI_RESET "\x1b[0m"
+
+void testMutantStackBasic() {
+    std::cout << ANSI_CYAN << "Basic tests" << ANSI_RESET << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(2);
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(7);
+
+    std::cout << "size: " << mstack.size() << std::endl;
+    std::cout << "empty() : " << mstack.empty() << std::endl;
+    std::cout << "top node: " << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << "top node after pop: " << mstack.top() << std::endl;
+    std::cout << "popping remainder..." << std::endl;
+    mstack.pop();
+    mstack.pop();
+    mstack.pop();
+    std::cout << "size: " << mstack.size() << std::endl;
+    std::cout << "empty() : " << mstack.empty() << std::endl;
+}
+
+
 int main()
 {
+    testMutantStackBasic();
+
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
