3f8018561a20245c5465eb878e07642a1c61ea8d
Christiaan Herrewijn <cherrewi@student.codam.nl>
2024-05-05 19:17:02 +0200

    added binaryListInsertion function

--- module_09/ex02/main.cpp
+++ module_09/ex02/main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <sstream>
 #include <regex>
+#include <iterator>
 
 #include "PmergeMe.hpp"
 
@@ -23,6 +24,33 @@ unsigned int getJacobstahlNr(unsigned int n)
     return (n % 2 == 0) ? ((1UL<<n)-1)/3 : ((1UL<<n)+1)/3;
 }
 
+
+/*
+NOTE:
+- when calling this function, 'maxIndex' should be lst.size().
+- this function assumes the list is sorted
+*/
+void binaryListInsertion(int nrToInsert, std::list<int> &lst, int minIndex,
+                         int maxIndex)
+{
+    if (minIndex == maxIndex) {
+        auto it = lst.begin();
+        std::advance(it, minIndex);
+        lst.insert(it, nrToInsert);
+    }
+    else {
+        int midIndex = minIndex + (maxIndex - minIndex) / 2;
+        auto itMid = lst.begin();
+        std::advance(itMid, midIndex);
+        if (nrToInsert < *itMid) {
+            binaryListInsertion(nrToInsert, lst, minIndex, midIndex);
+        }
+        else {
+            binaryListInsertion(nrToInsert, lst, midIndex+1, maxIndex);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     PmergeMe sorter;
@@ -35,6 +63,21 @@ int main(int argc, char *argv[])
     for (auto pair : sorter._pairList) {
         std::cout << pair.first << ", " << pair.second << std::endl;
     }
+
+    // test binary list insertion
+    std::list<int> lst;
+    lst.emplace_back(3);
+    lst.emplace_back(3);
+    lst.emplace_back(5);
+    lst.emplace_back(7);
+    lst.emplace_back(100);
+
+    auto it = lst.begin();
+    binaryListInsertion(42, lst, 0, lst.size());
+    for (int num : lst) {
+        std::cout << num << ", ";
+    }
+    std::cout << std::endl;
 
     // test parsing
     std::cout << sorter._inputList.size() << std::endl;
